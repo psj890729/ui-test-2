ef49c24cf95f5153f85e67fcf43ee89c95f4c223
psj890729 <psj890729@naver.com>
2018-02-11 16:54:17 +0900

    #1 close webdrivermanager

--- src/main/java/com/suji/test/ui/BaseTestClass.java
+++ src/main/java/com/suji/test/ui/BaseTestClass.java
@@ -1,12 +1,22 @@
 package com.suji.test.ui;
 
 import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.codeborne.selenide.WebDriverRunner;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTestClass {
-	
+		
 	@BeforeClass
 	public static void setupConfig() throws Exception {
-		System.setProperty("selenide.browser", "chrome");
-		System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        WebDriverRunner.setWebDriver(driver);
+		//System.setProperty("selenide.browser", "chrome");
+		//System.setProperty("webdriver.chrome.driver", "chromedriver");
 	}
 }
