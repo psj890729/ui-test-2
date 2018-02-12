decaddff76680cc0ee52946812701fbdfdabb4ac
psj890729 <psj890729@naver.com>
2018-02-12 10:04:49 +0900

    quit webdriver

--- src/main/java/com/suji/test/ui/BaseTestClass.java
+++ src/main/java/com/suji/test/ui/BaseTestClass.java
@@ -1,5 +1,6 @@
 package com.suji.test.ui;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,13 +11,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTestClass {
 		
+	private static WebDriver driver;
+	
 	@BeforeClass
 	public static void setupConfig() throws Exception {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         WebDriverRunner.setWebDriver(driver);
 		//System.setProperty("selenide.browser", "chrome");
 		//System.setProperty("webdriver.chrome.driver", "chromedriver");
+	}
+	
+	@AfterClass
+	public static void teardown() throws Exception {
+		driver.quit();
 	}
 }
