86d344eb4e276ce16eb6682c9c63db3fb5d8981c
psj890729 <psj890729@naver.com>
2018-03-06 16:31:06 +0900

    test

--- src/main/java/com/suji/test/ui/BaseTestClass.java
+++ src/main/java/com/suji/test/ui/BaseTestClass.java
@@ -11,10 +11,13 @@ public class BaseTestClass {
 	public ScreenShooter screenShooter = ScreenShooter.failedTests();
 	
 	@BeforeClass
-	public static void setupConfig() throws Exception {
+	public static void setupConfig() throws Exception {		
 		com.codeborne.selenide.Configuration.browser = "chrome";
-		
 		com.codeborne.selenide.Configuration.baseUrl = "https://www.google.co.kr";
+
+		System.out.println("report folder : " + com.codeborne.selenide.Configuration.reportsFolder);
+		System.out.println("report url : " + com.codeborne.selenide.Configuration.reportsUrl);
+		System.out.println("selenide.browser : " + System.getProperty("selenide.browser"));
 	}
 	
 }
