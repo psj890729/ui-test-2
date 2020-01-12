1f4245cb35bdc9c13d4bf6f726f1362caa0479fc
psj890729 <psj890729@gmail.com>
2020-01-12 20:42:23 +0900

    url 없음으로 테스트 제거

--- src/test/java/test/web/common/ZindexTest.java
+++ src/test/java/test/web/common/ZindexTest.java
@@ -29,17 +29,6 @@ public class ZindexTest {
 		assertThat(getFailCount(elements, 9999999), equalTo(0));
 	}
 	
-	@Test
-	public void a1MainPageTest() throws Exception {
-		String url = "https://a1.nexon.com/main/index";
-		SelenideElement visibleElement = $(".midArea");
-		
-		ElementsCollection elements = ZindexSeletor.getAllZindexElements(url, visibleElement);
-		
-		assertThat(getFailCount(elements, 9999999), equalTo(0));
-		
-	}
-	
 	private int getFailCount(ElementsCollection elements, int no) {
 		int failCount = 0;
 		for(SelenideElement element : elements) {
